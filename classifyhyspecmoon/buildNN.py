ac9c7b93a02591585fa6a1e0b2a02f9f8a8a50cb
Z.M. Vig <zvig@umd.edu>
2024-03-28 10:38:37 -0400

    Model Testing

--- classifyhyspecmoon/buildNN.py
+++ classifyhyspecmoon/buildNN.py
@@ -20,11 +20,12 @@ class MyHyperParameters():
         batch_size // Number of entries (pixels) seen at a time
         nepochs // Number of times the model sees the training data
     """
-    def __init__(self,nlyrs:'float',npl:'list',lrate:'float') -> None:
+    def __init__(self,nlyrs:'float',npl:'list',lrate:'float',batch_size:'float',nepochs:'float') -> None:
         self.nlyrs = nlyrs
         self.npl = npl
         self.lrate = lrate
-
+        self.batch_size = batch_size
+        self.nepochs = nepochs
 
 
 def run_mlp(
@@ -37,7 +38,7 @@ def run_mlp(
 
     model = keras.Sequential()
 
-    model.add(keras.Input(shape=(data.num_pixels,),name='Input_Layer'))
+    model.add(keras.Input(shape=(data.X_train.shape[0],),name='Input_Layer'))
 
     for i in range(0,hpms.nlyrs):
         model.add(keras.layers.Dense(units=hpms.npl[i],activation=keras.activations.relu))
@@ -49,4 +50,18 @@ def run_mlp(
         loss='mse'
     )
 
-    return model    
+    history = model.fit(
+    x=np.moveaxis(data.X_train,0,1),
+    y=np.moveaxis(data.Y_train,0,1),
+    batch_size=hpms.batch_size,
+    epochs=hpms.nepochs,
+    verbose='auto',
+    validation_split=0.1,
+    validation_data=None,
+    shuffle=True,
+    class_weight=None,
+    sample_weight=None,
+    initial_epoch=0,
+    )
+
+    return model
--- classifyhyspecmoon/neuralnetdata.py
+++ classifyhyspecmoon/neuralnetdata.py
@@ -4,6 +4,9 @@ Here we will load the data required for our neural net. Processing is available 
 import keras
 import numpy as np
 import h5py as h5
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
 from .create_labels import spa_label
 
 def descend_obj(obj,sep='\t'):
@@ -45,6 +48,7 @@ class NeuralNetData():
 
             self.rawspec = include_data(f,'VectorDatasets/RawSpectra',flip=True)
             self.smoothspec = include_data(f,'VectorDatasets/SmoothSpectra_GNDTRU',flip=True)
+            self.X = np.moveaxis(self.smoothspec.reshape(self.smoothspec.shape[0],self.smoothspec.shape[1]*self.smoothspec.shape[2]),0,1)
             self.shadowmap = include_data(f,'ShadowMaps/lowsignal_shadows',flip='y')
 
             self.wvl = f.attrs['smooth_wavelengths']
@@ -84,17 +88,28 @@ class NeuralNetData():
         self.labeled_data[background_label==0] = n
         label_dict['background'] = n
         self.labeled_data[self.shadowmap==1] = 0
+        self.Y = self.labeled_data.flatten()
         
         self.num_labels = len(np.unique(self.labeled_data))
 
-    def minmax_normalization(self):
+    def split_train_test(self) -> None:
         """
-        Method for normalizing the data from 0 to 1
+        Method for splitting our data into testing and training sets
         """
-        self.labeled_data 
+        self.X_train,self.X_test,self.Y_train,self.Y_test = train_test_split(self.X,self.Y,test_size=0.25,random_state=42)
+
+    def minmax_normalization(self,minmaxrange:'tuple') -> None:
+        """
+        Method for normalizing the data to the minmaxrange
+        """
+        scaler = MinMaxScaler(minmaxrange,copy=False)
+        scaler.fit(self.X_train)
+        
+        scaler.transform(self.X_train)
+        scaler.transform(self.X_test)
+        
     
-    def split_train_test(self):
-        pass
+
     
 
         
